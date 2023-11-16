b48d49db674d18eb25574c402938b6ecc61131f1
ammar abdurahman <33496831+ammarabdurahman@users.noreply.github.com>
2023-11-16 15:30:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '801da4be63msh42892b1e00b9a5cp1d9ed4jsn80b658a037fb', //Your API Key
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city)=>{ 
+cityName.innerHTML = city 
+fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+    .then(response => response.json())
+    .then((response) => {
+        console.log(response)
+
+        temp.innerHTML = response.temp
+        temp2.innerHTML = response.temp
+        humidity.innerHTML = response.humidity
+        humidity2.innerHTML = response.humidity
+        min_temp.innerHTML = response.min_temp
+        max_temp.innerHTML = response.max_temp
+        wind_speed.innerHTML = response.wind_speed
+        wind_speed2.innerHTML = response.wind_speed
+        
+        const sunriseUTC = new Date(response.sunrise);
+        const sunsetUTC = new Date(response.sunset);
+
+        // Format Waktu
+        const options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          };
+
+        sunrise.innerHTML = sunriseUTC.toLocaleTimeString('en-US', options);
+        sunset.innerHTML = sunsetUTC.toLocaleTimeString('en-US', options);
+    })
+    .catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    getWeather(city.value)
+})
+getWeather("Jakarta")
